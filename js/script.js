1f2176e25d57767418100d7ce6a4f2f9fe42caca
Artem Pronenko <pronenko.artem@gmail.com>
2020-04-27 18:38:40 +0300

    rewrote on OOP

--- js/script.js
+++ js/script.js
@@ -1,42 +1,57 @@
 //
 'use strict';
-let noteId = 8, columnId = 4;
 
-const addNotes = columnEl => {
-	const btnAddNote = columnEl.querySelector('[data-action-addNote]');
-	btnAddNote.addEventListener('click', evt => {
-		const note = document.createElement('div');
-		note.classList.add('note');
-		note.setAttribute('draggable', 'true');
-		note.setAttribute('data-note-id', noteId.toString());
-		noteId++;
-		columnEl.querySelector('[data-notes]').append(note);
-	});
-};
-document.querySelectorAll('.column').forEach(addNotes);
+class Trello {
+	constructor({noteId, columnId}) {
+		this.noteId = noteId;
+		this.columnId = columnId;
+	}
 
+	init() {
+		document.querySelectorAll('.column').forEach(this.addNotes.bind(this));
+		this.addColumn();
+	}
 
-const addColumn = () => {
-	const btnAddColumn = document.querySelector('[data-action-addColumn]'),
-		columnHTML = `
-		<p class="column-header">В плане</p>
-		<div data-notes></div>
-		<p class="column-footer">
-			<span data-action-addNote class="action">+ Добавить карточку</span>
-		</p>
-	`;
-	btnAddColumn.addEventListener('click', evt => {
-		const column = document.createElement('div');
-		column.classList.add('column');
-		column.setAttribute('draggable', 'true');
-		column.setAttribute('data-column-id', columnId.toString());
-		columnId++;
-		column.innerHTML = columnHTML;
-		document.querySelector('[data-columns-list]').append(column);
-		addNotes(column);
-	});
-};
-addColumn();
+	addNotes(columnEl) {
+		const btnAddNote = columnEl.querySelector('[data-action-addNote]');
+		btnAddNote.addEventListener('click', evt => {
+			const note = document.createElement('div');
+			note.classList.add('note');
+			note.setAttribute('draggable', 'true');
+			note.setAttribute('data-note-id', this.noteId.toString());
+			this.noteId++;
+			columnEl.querySelector('[data-notes]').append(note);
+		});
+	}
+
+	addColumn() {
+		const btnAddColumn = document.querySelector('[data-action-addColumn]'),
+			columnHTML = `
+				<p class="column-header">В плане</p>
+				<div data-notes></div>
+				<p class="column-footer">
+					<span data-action-addNote class="action">+ Добавить карточку</span>
+				</p>
+			`;
+		btnAddColumn.addEventListener('click', evt => {
+			const column = document.createElement('div');
+			column.classList.add('column');
+			column.setAttribute('draggable', 'true');
+			column.setAttribute('data-column-id', this.columnId.toString());
+			this.columnId++;
+			column.innerHTML = columnHTML;
+			document.querySelector('[data-columns-list]').append(column);
+			this.addNotes(column);
+		});
+	}
+
+}
+
+const trello = new Trello({
+	noteId: 8,
+	columnId: 4
+});
+trello.init();
 
 
 
